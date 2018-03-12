aec4289fdaa0f30ae8fd9621a760ba7f0feb32b1
Spencer Dangel <dangelspencer@gmail.com>
2018-03-12 08:39:47 -0400

    REP-79 CRUD updates for hosts

--- src/components/dashboard/dashboard.ts
+++ src/components/dashboard/dashboard.ts
@@ -1,15 +1,27 @@
 import {autoinject} from 'aurelia-framework';
+import {DialogService} from 'aurelia-dialog';
 
 import {DashboardService} from "../../services/dashboardService";
+import {EditHostDialog} from '../hosts/editHost';
 
 @autoinject()
 export class Dashboard {
     
     public nodes: any;
     
-    constructor(private dashboardService: DashboardService){ }
+    constructor(private dashboardService: DashboardService, private dialogService: DialogService){ }
 
     async bind() {
+        await this.load_hosts();
+    }
+
+    async load_hosts() {
         this.nodes = await this.dashboardService.getDashboardData();
+    }
+
+    async create_host() {
+        this.dialogService.open({viewModel: EditHostDialog, model: null, lock: false}).whenClosed(async response => {
+            await this.load_hosts();
+        });
     }
 }

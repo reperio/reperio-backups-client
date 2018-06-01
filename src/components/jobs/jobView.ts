b072d0e3547d601cca21d00f19478b4aaa06a4af
Spencer Dangel <dangelspencer@gmail.com>
2018-06-01 09:19:23 -0400

    REP-74 Remove unnecessary functions and console logs

--- src/components/jobs/jobView.ts
+++ src/components/jobs/jobView.ts
@@ -85,7 +85,7 @@ export class JobView {
     }
 
     add_action_handlers() {
-        setTimeout(() => {
+        setInterval(() => {
             const editElements: any = this.element.querySelectorAll('.edit-icon');
             for(let i = 0; i < editElements.length; i++) {
                 editElements[i].onclick = (params) => {
@@ -99,7 +99,7 @@ export class JobView {
                     this.delete_job(params.srcElement.dataset.job_id);
                 };
             }
-        }, 500);
+        }, 100);
     }
 
     activate(params, queryString, routeConfig) {
@@ -208,97 +208,12 @@ export class JobView {
     }
 
     public onReady($event) {
-        console.log('onReady');
-        console.log($event);
         this.calculateRowCount();
     }
 
-    private onCellClicked($event) {
-    }
-
-    private onCellValueChanged($event) {
-        console.log('onCellValueChanged: ' + $event.oldValue + ' to ' + $event.newValue);
-    }
-
-    private onRowValueChanged($event) {
-        console.log('onRowValueChanged: ' + $event.oldValue + ' to ' + $event.newValue);
-    }
-
-    private onCellDoubleClicked($event) {
-    }
-
-    private onCellContextMenu($event) {
-    }
-
-    private onCellFocused($event) {
-    }
-
-    private onRowSelected($event) {
-        // taking out, as when we 'select all', it prints to much to the console!!
-        // console.log('onRowSelected: ' + $event.node.data.name);
-    }
-
-    private onSelectionChanged() {
-    }
-
-    private onBeforeFilterChanged() {
-    }
-
-    private onAfterFilterChanged() {
-    }
-
-    private onFilterModified() {
-        console.log('filter changed');
-    }
-
-    private onBeforeSortChanged() {
-    }
-
-    private onAfterSortChanged() {
-        console.log('sort changed');
-    }
-
-    private onVirtualRowRemoved($event) {
-        // because this event gets fired LOTS of times, we don't print it to the
-        // console. if you want to see it, just uncomment out this line
-        // console.log('onVirtualRowRemoved: ' + $event.rowIndex);
-    }
-
-    private onRowClicked($event) {
-    }
 
     private onQuickFilterChanged($event) {
         this.gridOptions.api.setQuickFilter($event.target.value);
-    }
-
-    // here we use one generic event to handle all the column type events.
-    // the method just prints the event name
-    private onColumnEvent($event) {
-    }
-
-    private statusRenderer(params) {
-        
-        const styles = {
-            0: 'r-job-history-pending',
-            1: 'r-job-history-active',
-            2: 'r-job-history-success',
-            3: 'r-job-history-failed',
-        };
-        
-        const template = `<div class='r-job-history-status ${styles[params.value]}'>&nbsp;</div>`;
-        return template;
-    }
-
-    private snapshotStatusRenderer(params) {
-        const status_map = {
-            0: 'default',
-            1: 'created',
-            2: 'deleted',
-            3: 'failed',
-            4: 'receive_failed'
-        };
-
-        return status_map[params.value];
     }
 
     private actionsRenderer(params) {
@@ -332,7 +247,6 @@ export class JobView {
                     toastr.error('Failed to update job');
                 }
                 this.api.setDatasource(this.dataSource);
-                this.add_action_handlers();
             }
         });
     }
@@ -348,7 +262,6 @@ export class JobView {
                 }
                 
                 this.api.setDatasource(this.dataSource);
-                this.add_action_handlers();
             }
         });
     }
@@ -393,164 +306,3 @@ export class JobView {
         });
     }
 }
-
-
-
-// import {autoinject} from 'aurelia-framework';
-// import {DialogService} from 'aurelia-dialog';
-// import * as toastr from 'toastr';
-// import * as $ from 'jquery';
-// import * as _ from 'lodash';
-// import {Host} from '../../models/host';
-// import {HostService} from '../../services/hostService';
-// import {Job} from '../../models/job';
-// import {JobService} from '../../services/jobService';
-// import {DeleteDialog} from '../dialogs/deleteDialog';
-// import {JobCreateDetailsDialog} from './create/jobCreateDetailsDialog';
-// import {JobCreateRetentionDialog} from './create/jobCreateRetentionDialog';
-// import {JobEditDialog} from './edit/jobEditDialog';
-// import {JobScheduleDialog} from './create/jobCreateScheduleDialog';
-// import {VirtualMachine} from '../../models/virtual_machine';
-// import {VirtualMachineService} from '../../services/virtualMachineService';
-
-// @autoinject()
-// export class JobView {
-//     public jobs: Job[];
-//     public hosts: Host[];
-//     public filtered_virtual_machines: VirtualMachine[];
-//     public virtual_machines: VirtualMachine[];
-//     public selected_host: Host;
-//     public table: any;
-//     public $displayData: any[];
-
-//     public filters: any[] = [
-//         { value: '', keys: ['job_source_host.id'] },
-//         { value: '', keys: ['job_virtual_machine.id'] }
-//     ];
-
-//     constructor(private dialogService: DialogService, private jobService: JobService, private hostService: HostService, private virtualMachineService: VirtualMachineService) { }
-
-//     async bind() {
-//         await this.load_jobs();
-//         this.hosts = await this.hostService.get_hosts();
-//         this.virtual_machines = await this.virtualMachineService.getVirtualMachines();
-//         this.filtered_virtual_machines = this.virtual_machines;
-        
-
-//         //data.bind: jobs; display-data.bind: $displayData; filters.bind: filters
-//     }
-
-//     async attached() {
-//         console.log(this.table);
-//     }
-
-//     async load_jobs() {
-//         this.jobs = await this.jobService.get_jobs();
-//     }
-
-//     async update_job_enabled_status(id: string, job: Job) {
-//         await this.jobService.update_job(id, job);
-//         await this.load_jobs();
-//     }
-
-//     async update_selected_host() {
-//         this.hosts.forEach(host => {
-//             if (host.id === this.filters[0].value) this.selected_host = host;
-//         });
-//         this.filters[1].value = '';
-//         console.log(JSON.stringify(this.filters[0].value));
-//         if (this.selected_host === null || typeof this.selected_host === 'undefined') {
-//             console.log('no filter');
-//             this.filtered_virtual_machines = this.virtual_machines;
-//         } else {
-//             console.log('filter');
-//             this.filtered_virtual_machines = this.virtual_machines.filter(vm => {
-//                 return vm.host_id === this.selected_host.sdc_id;
-//             });
-//         }
-
-//         this.filtered_virtual_machines.sort((a, b) => {
-//             const a_value = a.name == '' ? a.id.toUpperCase() : a.name.toUpperCase();
-//             const b_value = b.name == '' ? b.id.toUpperCase() : b.name.toUpperCase();
-
-//             return a_value < b_value ? -1 : 1;
-//         });
-//     }
-
-//     update_sort_column() {
-//         console.log(this.table);
-//         if (this.filters[1].value !== ''){
-//             this.table.sortKey = 'name';
-//             $('#vm_header').removeClass('aut-asc');
-//             $('#vm_header').removeClass('aut-desc');
-//             $('#vm_header').addClass('aut-sortable');
-//             $('#job_header').addClass('aut-asc');
-//         } else {
-//             $('#job_header').removeClass('aut-asc');
-//             $('#job_header').removeClass('aut-desc');
-//             $('#job_header').addClass('aut-sortable');
-//             $('#vm_header').addClass('aut-asc');
-//             this.table.sortKey = 'job_virtual_machine.name';
-//         }
-//         this.table.doSort(this.$displayData);
-//     }
-
-//     async edit_job(job){
-//         this.dialogService.open({viewModel: JobEditDialog, model: $.extend( {}, job), lock: false}).whenClosed(async (response) => {
-//             if (!response.wasCancelled) {
-//                 const updated_job = response.output;
-//                 await this.jobService.update_job(updated_job.id, updated_job);
-//                 toastr.success('Job updated');
-//                 await this.load_jobs();
-//             }
-//         });
-//     }
-
-//     async delete_job(id: string) {
-//         this.dialogService.open({viewModel: DeleteDialog, model: 'Are you sure you want to delete this job?', lock: false}).whenClosed(async (response) => {
-//             if (!response.wasCancelled) {
-//                 await this.jobService.delete_job(id);
-//                 await this.load_jobs();
-//             }
-//         });
-//     }
-
-//     async create_job(step_number: number, job: Job) {
-//        if(step_number === 1) {
-//            this.open_job_details_modal(job);
-//        } else if (step_number === 2) {
-//            this.open_job_schedule_modal(job);
-//        } else if (step_number === 3) {
-//            this.open_job_retention_modal(job);
-//        }
-//     }
-
-//     async open_job_details_modal(job) {
-//         this.dialogService.open({viewModel: JobCreateDetailsDialog, model: job, lock: false}).whenClosed(async (response) => {
-//             if (!response.wasCancelled) {
-//                 return this.create_job(2, response.output);
-//             }
-//             return null;
-//         });
-//     }
-
-//     async open_job_schedule_modal(job) {
-//         this.dialogService.open({viewModel: JobScheduleDialog, model: job, lock: false}).whenClosed(async (response) => {
-//             if (!response.wasCancelled) {
-//                 return this.create_job(3, response.output);
-//             }
-//             return this.create_job(1, response.output);
-//         });
-//     }
-
-//     async open_job_retention_modal(job) {
-//         this.dialogService.open({viewModel: JobCreateRetentionDialog, model: job, lock: false}).whenClosed(async (response) => {
-//             if (!response.wasCancelled) {
-//                 await this.load_jobs();
-//                 toastr.success(`Job "${response.output.name}" was created successfully`);
-//                 return;
-//             }
-//             return this.create_job(2, response.output);
-//         });
-//     }
-// }
